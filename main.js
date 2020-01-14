58798820f672a5e8a3d630e9eec1dab709328889
Gwanghyeon Seo <dya2122@gmail.com>
2020-01-13 21:55:18 -0600

    1D1C 0113 URL Path 방식으로 parameter를 처리하는 라우팅 기법 적용

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = express();  //application을 얻는 방법
 const port = 3000;
 var fs = require('fs');
 var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 // get은 rounte, routing =>  어떤 길을 따라서 갈림길에서 적당한 곳으로 방향을 잡는 것, 즉 사용자들이 
 //여러 경로를 통해서 들오면 적절히 바꿔 주는 것
@@ -19,10 +21,40 @@ app.get('/', function(request, response) {
     response.send(html);
   });
 });
-
-app.get('/page', function(req, res) {
-  return res.send('/page')
+// 사용자가 주는 어떤 값에다가 pageId를 할당한다.
+// url path 방식으로 parameter를 전달하는 것을 처리하는 라우팅 기법을 배움
+app.get('/page/:pageId', function(request, response) {
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/page/${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
 });
+
+//2개 이상 parameter 주는 경우
+/*
+app.get('/page/:pageId/:chapterid', function(request, res) {
+  return res.send(request.params.pageId);
+
+});
+*/
+
 
 // 3000포트에 리스팅 한다.
 // app.listen이랑 동일한 역할을 한다.
@@ -45,17 +77,6 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
       } else {
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
