ee1e92c3387941e45163e8c3ca7a6d69a962897a
GwanghyeonSeo <dya2122@gmail.com>
2020-02-06 17:55:09 -0600

    1D1C 0206 Mongo DB 넣는중
    Mongo DB 넣는중

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,28 @@ var express = require('express');
 var router = express.Router();
 
 var template = require('../lib/template.js');
+var  MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://user:pwd@amazon:27017/test';
+
+var db;
+
 
 // get은 rounte, routing =>  어떤 길을 따라서 갈림길에서 적당한 곳으로 방향을 잡는 것, 즉 사용자들이 
 //여러 경로를 통해서 들오면 적절히 바꿔 주는 것
 //app.get('/', (req, res) => res.send('Hello World!'))//  
 router.get('/', function(request, response) {
+    MongoClient.connect(url,{ useUnifiedTopology: true} , function (err, database) {
+      if (err) {
+          console.error('MongoDB 연결 실패', err);
+          return;
+      }
+      db = database;
+      var movies = db.Collection('test');
+     
+    });
+
+    
+
     var title = 'Welcome';
     var description = 'Hello, Node.js';
     var list = template.list(request.flist);
